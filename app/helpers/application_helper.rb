c35fdb23c5ab25fe1e112eb903eac171804803c2
milen-ale <milenalebel2@gmail.com>
2022-12-29 12:54:43 +0300

    modify expense controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def foo_helper(&block)
-    FooPresenter.new(self, expense: expense).render(&block)
+  def foo_helper(&)
+    FooPresenter.new(self, expense:).render(&)
   end
 end
